a76709ce235f23be8b9d81937b84a2ce0c3a88cb
ToshaEX <tharinduoshan@gmail.com>
2023-01-31 00:51:38 +0530

    create appointment

--- src/views/Booking/BookingPages/TimePicker.jsx
+++ src/views/Booking/BookingPages/TimePicker.jsx
@@ -1,18 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Calendar from "react-calendar";
+import { slotsList } from "./slots";
+import { useSelector } from "react-redux";
 import "react-calendar/dist/Calendar.css";
 
+const dateFormatter = (date, isAdd = 0) => {
+  const year = date.getFullYear();
+  const day = date.getDate() + (isAdd === 0 ? 0 : isAdd === 1 ? 1 : -1);
+  const month = date.getMonth() + 1;
+  return year + "-" + month + "-" + day;
+};
+
+const slotFactory = (startSlot, slotAmount) => {
+  const slots = [];
+  for (let i = startSlot + 1; i <= slotAmount + startSlot; i++) {
+    slots.push(i);
+  }
+  return slots;
+};
+
 function TimePicker(props) {
-  const [date, setDate] = useState(new Date());
-  const [beautician, setBeautician] = useState([]);
-
+  const { timeTaken, handleTime } = props;
+  //option
+  const userId = useSelector((state) => state.user.userId);
+  const [beauticianOption, setBeauticianOption] = useState([]);
+  const [date, setDate] = useState(null);
   const [selectedBeautician, setSelectedBeautician] = useState(null);
-  const [slotsData, setSlotsData] = useState([]);
-  const [slots, setSlots] = useState([]);
+  const [beauticianSlots, setBeauticianSlots] = useState(null);
+  const [selectedSlots, setSelectedSlots] = useState([]);
 
-  const [slotList, setSlotList] = useState([]);
-  const [slotNumber, setSlotNumber] = useState([]);
+  const [dateSelection, setDateSelection] = useState({
+    startDate: date,
+    endDate: date,
+  });
 
   useEffect(() => {
     axios({
@@ -21,102 +42,57 @@ function TimePicker(props) {
       responseType: "json",
     })
       .then((res) => {
-        setBeautician(res.data);
+        setBeauticianOption(res.data);
       })
       .catch((err) => {
         console.log("Failed to load Services", err);
       });
   }, []);
+  console.log(userId);
 
-  async function getSlots(beauticianID) {
-    setSlotList([]);
-    setSlotNumber([]);
-
-    await axios({
-      method: "GET",
-      url: `appointment/user/${beauticianID}/slots`,
-      responseType: "json",
-    })
-      .then((res) => {
-        setSlotsData(res.data);
-        displaySlots(beauticianID);
-        console.log(res.data);
-        console.log(slots);
-      })
-      .catch((err) => {
-        console.log("Failed to load Services", err);
-      });
-  }
-
-  const slotsList = [
-    { number: 1, timeSlot: "8.00 am - 8.15 am" },
-    { number: 2, timeSlot: "8.15 am - 8.30 am" },
-    { number: 3, timeSlot: "8.30 am - 8.45 am" },
-    { number: 4, timeSlot: "8.45 am - 9.00 am" },
-    { number: 5, timeSlot: "9.00 am - 9.15 am" },
-    { number: 6, timeSlot: "9.15 am - 9.30 am" },
-    { number: 7, timeSlot: "9.30 am - 9.45 am" },
-    { number: 8, timeSlot: "9.45 am - 10.00 am" },
-    { number: 9, timeSlot: "10.00 am - 10.15 am" },
-    { number: 10, timeSlot: "10.15 am - 10.30 am" },
-    { number: 11, timeSlot: "10.30 am - 10.45 am" },
-    { number: 12, timeSlot: "10.45 am - 11.00 am" },
-    { number: 13, timeSlot: "11.00 am - 11.15 am" },
-    { number: 14, timeSlot: "11.15 am - 11.30 am" },
-    { number: 15, timeSlot: "11.30 am - 11.45 am" },
-    { number: 16, timeSlot: "11.45 am - 12.00 pm" },
-    { number: 17, timeSlot: "1.00 pm - 1.15 pm" },
-    { number: 18, timeSlot: "1.15 pm - 1.30 pm" },
-    { number: 19, timeSlot: "1.30 pm - 1.45 pm" },
-    { number: 20, timeSlot: "1.45 pm - 2.00 pm" },
-    { number: 21, timeSlot: "2.00 pm - 2.15 pm" },
-    { number: 22, timeSlot: "2.15 pm - 2.30 pm" },
-    { number: 23, timeSlot: "2.30 pm - 2.45 pm" },
-    { number: 24, timeSlot: "2.45 pm - 3.00 pm" },
-    { number: 25, timeSlot: "3.00 pm - 3.15 pm" },
-    { number: 26, timeSlot: "3.15 pm - 3.30 pm" },
-    { number: 27, timeSlot: "3.30 pm - 3.45 pm" },
-    { number: 28, timeSlot: "3.45 pm - 4.00 pm" },
-    { number: 29, timeSlot: "4.00 pm - 4.15 pm" },
-    { number: 30, timeSlot: "4.15 pm - 4.30 pm" },
-    { number: 31, timeSlot: "4.30 pm - 4.45 pm" },
-    { number: 32, timeSlot: "4.45 pm - 5.00 pm" },
-    { number: 33, timeSlot: "5.00 pm - 5.15 pm" },
-    { number: 34, timeSlot: "5.15 pm - 5.30 pm" },
-    { number: 35, timeSlot: "5.30 pm - 5.45 pm" },
-    { number: 36, timeSlot: "5.45 pm - 6.00 pm" },
-  ];
-
-  const amount = Math.round(props.timeTaken / 15);
-  const year = date.getFullYear();
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-
-  function displaySlots(b_id) {
-    setSlots((arr) =>
-      arr.concat(
-        slotsData
-          .filter((val) => val.startDate === month + " " + day)
-          .map((item, i) => ({ item }))
-      )
+  useEffect(() => {
+    console.log("effect run");
+    console.log(
+      dateSelection.endDate,
+      dateSelection.startDate,
+      selectedBeautician
     );
 
-    if (slots.length === 0) {
-      for (let i = 0; i < amount; i++) {
-        setSlotList((arr) => arr.concat(slotsList[i]));
-        setSlotNumber((arr) => arr.concat(i + 1));
-      }
-      setSelectedBeautician(b_id);
-      props.handleTime(true, year + "-" + month + "-" + day, b_id, slotNumber);
-    } else {
-      console.log("hiii");
-    }
+    if (
+      dateSelection.endDate !== null &&
+      dateSelection.startDate !== null &&
+      selectedBeautician !== null
+    ) {
+      console.log("effect body run");
+      axios({
+        method: "GET",
+        url: `appointment/user/${selectedBeautician}/slots/?startDate=${dateSelection.startDate}&endDate=${dateSelection.endDate}`,
+        responseType: "json",
+      })
+        .then((res) => {
+          setBeauticianSlots(res.data);
+          setSelectedSlots(
+            slotFactory(beauticianSlots?.at(-1), Math.ceil(timeTaken / 15))
+          );
+        })
 
-    // console.log(slotList.length);
-    // console.log(props.timeTaken);
-    // console.log(amount);
-    // console.log(date);
-  }
+        .catch((err) => {
+          console.log("Failed to load Services", err);
+        });
+    }
+  }, [date, selectedBeautician]);
+
+  useEffect(() => {
+    if (
+      date !== null &&
+      selectedBeautician !== null &&
+      selectedSlots.length !== 0
+    ) {
+      console.log("runnnnn");
+      handleTime(true, dateFormatter(date), selectedBeautician, selectedSlots);
+    }
+    console.log(selectedSlots);
+  }, [date, selectedBeautician, selectedSlots]);
 
   return (
     <div>
@@ -132,13 +108,13 @@ function TimePicker(props) {
               defaultValue={"none"}
               className="md:w-[20rem] w-[10rem] rounded-lg text-sm"
               onChange={(e) => {
-                getSlots(e.target.value);
+                setSelectedBeautician(e.target.value);
               }}
             >
               <option value={"none"} disabled>
                 Select a Beautician
               </option>
-              {beautician
+              {beauticianOption
                 .filter((val) => val.role === "beautician")
                 .map((item, index) => {
                   return (
@@ -155,7 +131,7 @@ function TimePicker(props) {
               Your Time Slots
             </div>
             <div className={selectedBeautician === null ? "hidden" : "block"}>
-              <div className="grid grid-cols-2 grid-rows-8 gap-2">
+              {/* <div className="grid grid-cols-2 grid-rows-8 gap-2">
                 {slotList.map((item, index) => {
                   return (
                     <div
@@ -166,14 +142,31 @@ function TimePicker(props) {
                     </div>
                   );
                 })}
-              </div>
+              </div> */}
+              {beauticianSlots && (
+                <div>
+                  {slotsList[beauticianSlots.at(-1)]?.timeSlot.split("-")[0]} -
+                  {slotsList[selectedSlots.at(-1)]?.timeSlot.split("-")[0]}
+                  {timeTaken}
+                </div>
+              )}
             </div>
           </div>
         </div>
         {/* calendar */}
         <div className="px-3 mt-5 mb-5 lg:mt-0">
           <div className="text-[14px] font-semibold pb-1">Pick a Date</div>
-          <Calendar value={date} onChange={setDate} />
+          <Calendar
+            value={date === null ? new Date() : date}
+            onChange={(e) => {
+              setDate(e);
+              setDateSelection({
+                startDate: dateFormatter(e, -1),
+                endDate: dateFormatter(e, 1),
+              });
+              console.log(date);
+            }}
+          />
         </div>
       </div>
     </div>
